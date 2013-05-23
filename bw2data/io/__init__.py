f6c6c9e000b9e73d947c07054a343543b12ece22
Chris Mutel <cmutel@gmail.com>
2013-05-23 10:37:08 +0200

    Don't import Ecospold2Importer by default yet

--- bw2data/io/__init__.py
+++ bw2data/io/__init__.py
@@ -3,4 +3,4 @@ from bw2package import BW2PackageExporter, BW2PackageImporter, \
 from export_gexf import DatabaseToGEXF
 from import_ecospold import Ecospold1Importer as EcospoldImporter
 from import_method import EcospoldImpactAssessmentImporter
-from import_ecospold2 import Ecospold2Importer
+# from import_ecospold2 import Ecospold2Importer
