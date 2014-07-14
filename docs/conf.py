335064aee1b6d52d27747bebcec957fb806b576e
Chris Mutel <cmutel@gmail.com>
2014-07-14 11:34:17 +0200

    1.0 RC3

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 # The short X.Y version.
 version = '1.0'
 # The full version, including alpha/beta/rc tags.
-release = '1.0 RC2'
+release = '1.0 RC3'
 
 import sys
 from os.path import abspath, dirname
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ for dirpath, dirnames, filenames in os.walk('bw2data'):
 
 setup(
     name='bw2data',
-    version="1.0RC2",
+    version="1.0RC3",
     packages=packages,
     author="Chris Mutel",
     author_email="cmutel@gmail.com",
