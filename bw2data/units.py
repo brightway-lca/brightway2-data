e1be76d30b65a65acd0c600eaaedf7d9fb911af4
Chris Mutel <cmutel@gmail.com>
2014-09-10 14:13:22 +0200

    Add some more ecoinvent units

--- bw2data/units.py
+++ bw2data/units.py
@@ -1,23 +1,25 @@
 UNITS_NORMALIZATION = {
     "ha": u"hectare",
+    "hr": u"hour",
     "kbq": u"kilo Becquerel",
     "kg": u"kilogram",
     "km": u"kilometer",
     "kwh": u"kilowatt hour",
+    "m*year": u"meter-year",
     "m2": u"square meter",
     "m2a": u"square meter-year",
     "m3": u"cubic meter",
+    "m3a": u"cubic meter-year",
     "ma": u"meter-year",
+    "metric ton*km": u"ton kilometer",
     "mj": u"megajoule",
+    "my": u"meter-year",
     "nm3": u"cubic meter",
+    "p": u"unit",
+    "personkm": u"person kilometer",
     "pkm": u"person kilometer",
     "tkm": u"ton kilometer",
     "vkm": u"vehicle kilometer",
-    # SimaPro units to convert, ranging from sensible to bizarre
-    "m3a": u"cubic meter-year",
-    "personkm": u"person kilometer",
-    "p": u"unit",
-    "my": u"meter-year",
 }
 
 normalize_units = lambda x: UNITS_NORMALIZATION.get(x.lower(), x)
