8369756d99c4c8860b19bfb4a8cfc77e447649d3
Chris Mutel <cmutel@gmail.com>
2015-05-13 12:04:25 +0200

    Mark dev version

--- bw2data/__init__.py
+++ bw2data/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = (2, 0, u"dev")
+__version__ = (2, 0, "dev0")
 
 from .project import projects
 from .configuration import config
--- setup.py
+++ setup.py
@@ -16,14 +16,14 @@ for dirpath, dirnames, filenames in os.walk('bw2data'):
 
 setup(
     name='bw2data',
-    version="2.0dev",
+    version="2.0.dev0",
     packages=packages,
     author="Chris Mutel",
     author_email="cmutel@gmail.com",
     license=open('LICENSE.txt').read(),
     install_requires=[
         "appdirs",
-        "bw2io",
+        "bw2io>=0.2.dev0",
         "docopt",
         "eight",
         "future",
