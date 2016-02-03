4b9b4a0a8be7aa160d3300c5aefa9c9edeea2cf3
Chris Mutel <cmutel@gmail.com>
2016-02-03 09:49:07 +0100

    Retrieve proper configuration code

--- bw2data/configuration.py
+++ bw2data/configuration.py
@@ -1,99 +1,80 @@
 # -*- coding: utf-8 -*-
-import codecs
+from __future__ import print_function, unicode_literals
+from eight import *
+
+import eight
 import json
 import os
 import platform
+import sys
 import tempfile
 import warnings
+from .filesystem import check_dir
 
-
-def _ran_from_ipython():
-    try:
-        __IPYTHON__
-        return True
-    except NameError:
-        return False
+# os.getenv returns unicode in Py2
+eight.wrap_os_environ_io()
 
 
 class Config(object):
-    """A singleton that store configuration settings. Default data directory is ``brightway2`` in the user's home directory, and is stored as ``config.dir``. Other configuration settings can also be assigned as needed.
-
-    Args:
-        * *path* (str, optional): The path of the data directory. Must be writeable.
-
-    """
-    version = 0.1
-    basic_directories = ("processed", "intermediate", "backups", "logs")
+    """A singleton that stores configuration settings"""
+    version = 3
     backends = {}
+    cache = {}
+    metadata = []
+    sqlite3_databases = []
     _windows = platform.system() == "Windows"
-    _ipython = _ran_from_ipython()
-
-    # TODO: Create directory if needed (and basic dirs)
-
-    def __init__(self, path=None):
-        self.is_temp_dir = False
-        self.reset(path)
-        self.cache = {}
-
-    def check_dir(self, directory=None):
-        """Returns ``True`` if given path is a directory and writeable, ``False`` otherwise. Default ``directory`` is the Brightway2 data directory."""
-        return os.path.isdir(directory or self.dir) and \
-            os.access(directory or self.dir, os.W_OK)
-
-    def reset(self, path=None):
-        """Reset to original configuration. Useful for testing."""
-        try:
-            self.dir = self.get_home_directory(path)
-            self.is_temp_dir = False
-        except OSError:
-            self.dir = tempfile.mkdtemp()
-            self.is_temp_dir = True
-            if not getattr(self, "dont_warn", False):
-                warnings.warn(u"Your changes will not be saved!\n"
-                              u"\tSet a writeable directory!\n"
-                              u"\tCurrent data directory is:\n"
-                              u"\t%s" % self.dir, UserWarning
-                              )
-            # Has to come here, because web interface wants
-            # to open a log ASAP
-            self.create_basic_directories()
-        self.load_preferences()
-
-    def load_preferences(self):
-        """Load a set of preferences from a file in the data directory.
-
-        Preferences as stored as ``config.p``."""
-        try:
-            self.p = json.load(open(os.path.join(
-                self.dir, "preferences.json")))
-        except:
-            self.p = {"use_cache": True}
-            self.save_preferences()
-
-    def save_preferences(self):
-        """Serialize preferences to disk."""
-        with open(os.path.join(
-                self.dir,
-                "preferences.json"), "w") as f:
-            json.dump(self.p, f, indent=2)
 
     @property
     def biosphere(self):
         """Get name for ``biosphere`` database from user preferences.
 
-        Default name is ``biosphere``; change this by changing ``config.p["biosphere_database"]``. Don't forget ``config.save_preferences()`` to save changes."""
-        if not hasattr(self, "p"):
-            self.load_preferences()
-        return self.p.get("biosphere_database", u"biosphere")
+        Default name is ``biosphere3``; change this by changing ``config.p["biosphere_database"]``."""
+        return self.p.get("biosphere_database", "biosphere3")
 
     @property
     def global_location(self):
         """Get name for global location from user preferences.
 
-        Default name is ``GLO``; change this by changing ``config.p["global_location"]``. Don't forget ``config.save_preferences()`` to save changes."""
-        if not hasattr(self, "p"):
-            self.load_preferences()
-        return self.p.get("global_location", u"GLO")
+        Default name is ``GLO``; change this by changing ``config.p["global_location"]``."""
+        return self.p.get("global_location", "GLO")
+
+    ### Compatibility layer for < 2.0
+
+    def reset(self, *args, **kwargs):
+        warnings.warn(
+            "`reset` is deprecated and doesn't do anything; please change "
+            "projects using `projects.current = 'foo'",
+            DeprecationWarning
+        )
+
+    @property
+    def dir(self):
+        warnings.warn(
+            "`config.dir` is deprecated; please use `projects.dir` instead",
+            DeprecationWarning
+        )
+
+    def check_dir(self, directory=None):
+        warnings.warn("`config.check_dir` is deprecated, please use `filesystem.check_dir`", DeprecationWarning)
+
+    def load_preferences(self):
+        warnings.warn(
+            "`config.load_preferences` is deprecated; preferences are loaded automatically",
+            DeprecationWarning
+        )
+
+    def save_preferences(self):
+        """Serialize preferences to disk."""
+        warnings.warn(
+            "`config.save_preferences` is deprecated; preferences are saved automatically",
+            DeprecationWarning
+        )
+
+    def request_dir(self, dirname):
+        warnings.warn(
+            "`config.request_dir` is deprecated; please use `projects.request_directory`",
+            DeprecationWarning
+        )
 
     def get_home_directory(self, path=None):
         """Get data directory, trying in order:
@@ -102,84 +83,47 @@ class Config(object):
         * ``BRIGHTWAY2_DIR`` environment variable
         * ``.brightway2path`` file in user's home directory
         * ``brightway2path.txt`` file in user's home directory
-        * ``brightway2`` in user's home directory
+        * ``brightway2data`` in user's home directory
 
         To set the environment variable:
 
         * Unix/Mac: ``export BRIGHTWAY2_DIR=/path/to/brightway2/directory``
-        * Windows XP: Instead of an environment variable, just create a ``brightway2path.txt`` file in your home directory (C:\Users\Your Name\) with a single line that is the directory path you want to use.
-        * Windows 7/8: ``setx BRIGHTWAY2_DIR=\path\\to\\brightway2\directory``
+        * Windows XP: Instead of an environment variable, just create a ``brightway2path.txt`` file in your home directory (C:\\Users\\Your Name\\) with a single line that is the directory path you want to use.
+        * Windows 7/8: ``setx BRIGHTWAY2_DIR=\\\\path\\\\to\\\\brightway2\\\\directory``
 
         """
-        if path:
-            self._dir_from = u"user provided"
-            return path
+        warnings.warn(
+            "`config.get_home_directory` is deprecated; the data directory is now managed automatically. Start a new project with `projects.current = 'my new project'`",
+            DeprecationWarning
+        )
         user_dir = os.path.expanduser("~")
         envvar = os.getenv("BRIGHTWAY2_DIR")
         if envvar:
-            self._dir_from = u"environment variable"
+            self._dir_from = "environment variable"
             if not self.check_dir(envvar):
-                warnings.warn(u"The environment variable BRIGHTWAY2_DIR was set, "
-                    u"but doesn't exist or is not writable."
+                warnings.warn("The environment variable BRIGHTWAY2_DIR was set, "
+                    "but doesn't exist or is not writable."
                 )
             return envvar
         for filename in (".brightway2path", "brightway2path.txt"):
             try:
-                candidate = codecs.open(
+                candidate = open(
                     os.path.join(user_dir, filename),
                     encoding='utf-8'
                 ).readline().strip()
-                if os.path.supports_unicode_filenames:
-                    # Leaving as unicode is OK
-                    pass
-                else:
-                    # posix systems except for Mac OS X
-                    candidate = candidate.encode('utf-8')
-                assert os.path.exists(candidate)
                 self._dir_from = filename
                 return candidate
             except:
                 pass
-        else:
-            self._dir_from = u"default"
-            return os.path.join(user_dir, "brightway2")
-
-    def request_dir(self, dirname):
-        """Return the absolute path to the subdirectory ``dirname``, creating it if necessary.
-
-        Returns ``False`` if directory can't be created."""
-        path = os.path.join(self.dir, dirname)
-        if self.check_dir(path):
-            return path
-        else:
-            try:
-                os.mkdir(path)
-                return path
-            except:
-                return False
+        self._dir_from = "default"
+        default_path = os.path.join(user_dir, "brightway2data")
+        if not os.path.exists(default_path):
+            os.mkdir(default_path)
+        return default_path
 
     def create_basic_directories(self):
-        """Create basic directory structure.
-
-        Useful when first starting or for tests."""
-        for name in self.basic_directories:
-            if not os.path.exists(os.path.join(self.dir, name)):
-                os.mkdir(os.path.join(self.dir, name))
-
-    def _get_dir(self):
-        # This is a string in the correct encoding for the filesystem
-        return self._dir
-
-    def _set_dir(self, d):
-        self._dir = d
-        if not self.check_dir():
-            raise OSError(u"This directory is not writeable")
-
-    dir = property(_get_dir, _set_dir)
-
-    @property
-    def udir(self):
-        """Return ``config.dir`` in Unicode"""
-        return self.dir.decode('utf-8')
+        warnings.warn("`config.create_basic_directories` is deprecated; "
+            "Basic directories are created automatically when starting a new project",
+            DeprecationWarning)
 
 config = Config()
