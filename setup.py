6d40847063152dc52ebbb21dfbfdeea4140cd730
Chris Mutel <cmutel@gmail.com>
2016-07-17 12:34:02 +0200

    Mark dev version as 2.3.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ for dirpath, dirnames, filenames in os.walk('bw2data'):
 
 setup(
     name='bw2data',
-    version="2.3.1",
+    version="2.3.2",
     packages=packages,
     author="Chris Mutel",
     author_email="cmutel@gmail.com",
