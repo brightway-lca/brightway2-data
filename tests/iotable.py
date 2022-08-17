1ab1888462620aba1c9a477ec7b821ea1200702f
Chris Mutel <cmutel@gmail.com>
2022-08-17 19:38:24 +0200

    Test IOTable dataframe construction

--- tests/iotable.py
+++ tests/iotable.py
@@ -1,8 +1,10 @@
-from bw2data import Method, Database, databases, methods, projects, get_activity
+from bw2data import Method, Database, databases, methods, projects, get_activity, get_id
 import pytest
 from bw2data.tests import bw2test
 from bw2calc import LCA
 import numpy as np
+from pandas.testing import assert_frame_equal
+import pandas as pd
 
 
 @pytest.fixture
@@ -29,14 +31,14 @@ def iotable_fixture():
     tech_exchanges = [
         ("a", "a", 2, False),
         ("a", "c", 3, True),
-        ("b", "a", 1, True),
-        ("b", "b", 1, False),
+        ("b", "a", -1, False),
+        ("b", "b", -1, True),
         ("c", "a", 4, False),
         ("c", "b", 0.2, True),
         ("c", "c", 1, True),
     ]
     bio_exchanges = [
-        ("d", "b", 1, False),
+        ("d", "b", -1, True),
         ("d", "c", 2, False),
     ]
 
@@ -56,7 +58,7 @@ def iotable_fixture():
                 "row": get_activity(code=m).id,
                 "col": get_activity(code=n).id,
                 "amount": o,
-                "flip": p,
+                "flip": p
             }
             for m, n, o, p in tech_exchanges
         ],
@@ -65,7 +67,7 @@ def iotable_fixture():
                 "row": get_activity(code=m).id,
                 "col": get_activity(code=n).id,
                 "amount": o,
-                "flip": p,
+                "flip": p
             }
             for m, n, o, p in bio_exchanges
         ],
@@ -94,27 +96,29 @@ def test_iotable_matrix_construction(iotable_fixture):
     tech_values = [
         ("a", "a", 2, False),
         ("a", "c", 3, True),
-        ("b", "a", 1, True),
-        ("b", "b", 1, False),
+        ("b", "a", -1, False),
+        ("b", "b", -1, True),
         ("c", "a", 4, False),
         ("c", "b", 0.2, True),
         ("c", "c", 1, True),
     ]
     for a, b, c, d in tech_values:
+        print(a, b, c, d)
+        print(lca.technosphere_matrix[
+                lca.dicts.product[get_activity(code=a).id],
+                lca.dicts.activity[get_activity(code=b).id],
+            ])
         assert np.allclose(
             lca.technosphere_matrix[
                 lca.dicts.product[get_activity(code=a).id],
                 lca.dicts.activity[get_activity(code=b).id],
             ],
-            (-1 if d else 1) * c
+            (-1 if d else 1) * c,
         )
-    assert np.allclose(
-        lca.technosphere_matrix.sum(),
-        2 - 3 - 1 + 1 + 4 - 0.2 - 1
-    )
+    assert np.allclose(lca.technosphere_matrix.sum(), 2 - 3 - 1 + 1 + 4 - 0.2 - 1)
 
     bio_values = [
-        ("d", "b", 1, False),
+        ("d", "b", -1, True),
         ("d", "c", 2, False),
     ]
     for a, b, c, d in bio_values:
@@ -123,17 +127,11 @@ def test_iotable_matrix_construction(iotable_fixture):
                 lca.dicts.biosphere[get_activity(code=a).id],
                 lca.dicts.activity[get_activity(code=b).id],
             ],
-            (-1 if d else 1) * c
+            (-1 if d else 1) * c,
         )
-    assert np.allclose(
-        lca.biosphere_matrix.sum(),
-        3
-    )
+    assert np.allclose(lca.biosphere_matrix.sum(), 3)
 
-    assert np.allclose(
-        lca.characterization_matrix.sum(),
-        42
-    )
+    assert np.allclose(lca.characterization_matrix.sum(), 42)
     assert lca.characterization_matrix.shape == (1, 1)
 
 
@@ -143,10 +141,133 @@ def test_iotable_process_method(iotable_fixture):
 
 def test_iotable_edges_to_dataframe(iotable_fixture):
     df = Database("cat").edges_to_dataframe()
+    id_map = {obj["code"]: obj.id for obj in Database("cat")} | {
+        obj["code"]: obj.id for obj in Database("mouse")
+    }
+
+    tech_exchanges = [
+        ("a", "a", 2, False),
+        ("a", "c", 3, True),
+        ("b", "a", -1, False),
+        ("b", "b", -1, True),
+        ("c", "a", 4, False),
+        ("c", "b", 0.2, True),
+        ("c", "c", 1, True),
+    ]
+    bio_exchanges = [
+        ("d", "b", -1, True),
+        ("d", "c", 2, False),
+    ]
+    expected = pd.DataFrame(
+        [
+            {
+                "target_id": id_map[a],
+                "target_database": "mouse" if a == "d" else "cat",
+                "target_code": a,
+                "target_name": get_activity(code=a).get("name"),
+                "target_reference_product": None,
+                "target_location": get_activity(code=a).get("location"),
+                "target_unit": get_activity(code=a).get("unit"),
+                "target_type": get_activity(code=a).get("type") or 'process',
+                "source_id": id_map[b],
+                "source_database": "mouse" if b == "d" else "cat",
+                "source_code": b,
+                "source_name": get_activity(code=b).get("name"),
+                "source_product": None,
+                "source_location": get_activity(code=b).get("location"),
+                "source_unit": get_activity(code=b).get("unit"),
+                "source_categories": None,
+                "edge_amount": c,
+                "edge_type": "technosphere" if ((-1 if d else 1) * c) < 0 else "production",
+            }
+            for b, a, c, d in tech_exchanges
+        ]
+        + [
+            {
+                "target_id": id_map[a],
+                "target_database": "mouse" if a == "d" else "cat",
+                "target_code": a,
+                "target_name": get_activity(code=a).get("name"),
+                "target_reference_product": None,
+                "target_location": get_activity(code=a).get("location"),
+                "target_unit": get_activity(code=a).get("unit"),
+                "target_type": get_activity(code=a).get("type") or 'process',
+                "source_id": id_map[b],
+                "source_database": "mouse" if b == "d" else "cat",
+                "source_code": b,
+                "source_name": get_activity(code=b).get("name"),
+                "source_product": None,
+                "source_location": get_activity(code=b).get("location"),
+                "source_unit": get_activity(code=b).get("unit"),
+                "source_categories": None,
+                "edge_amount": c,
+                "edge_type": "biosphere",
+            }
+            for b, a, c, d in bio_exchanges
+        ]
+    )
+
+    categorical_columns = [
+        "target_database",
+        "target_name",
+        "target_reference_product",
+        "target_location",
+        "target_unit",
+        "target_type",
+        "source_database",
+        "source_code",
+        "source_name",
+        "source_product",
+        "source_location",
+        "source_unit",
+        "source_categories",
+        "edge_type",
+    ]
+    for column in categorical_columns:
+        if column in expected.columns:
+            expected[column] = expected[column].astype("category")
+
+    assert_frame_equal(
+        df.sort_values(["target_id", "source_id"]).reset_index(drop=True),
+        expected.sort_values(["target_id", "source_id"]).reset_index(drop=True),
+        check_dtype=False,
+    )
 
 
 def test_iotable_nodes_to_dataframe(iotable_fixture):
     df = Database("cat").nodes_to_dataframe()
+    expected = pd.DataFrame(
+        [
+            {
+                "code": "a",
+                "database": "cat",
+                "id": get_id(("cat", "a")),
+                "location": "sunshine",
+                "name": "a",
+                "unit": "meow",
+            },
+            {
+                "code": "b",
+                "database": "cat",
+                "id": get_id(("cat", "b")),
+                "location": "curled up",
+                "name": "b",
+                "unit": "purr",
+            },
+            {
+                "code": "c",
+                "database": "cat",
+                "id": get_id(("cat", "c")),
+                "location": "on lap",
+                "name": "c",
+                "unit": "meow",
+            },
+        ]
+    )
+    assert_frame_equal(
+        df.reset_index(drop=True),
+        expected.reset_index(drop=True),
+    )
 
 
 # def test_production(activity_and_method):
@@ -178,7 +299,6 @@ def test_iotable_nodes_to_dataframe(iotable_fixture):
 #     assert len(activity.technosphere()) == 1
 #     exc = list(activity.technosphere())[0]
 #     assert exc["amount"] == 3
-
 
 
 # def test_readonlyexchange(activity_and_method):
