944ed3b39a8e83d32a98151416e6eb7f5e985ed7
Chris Mutel <cmutel@gmail.com>
2022-09-05 12:42:29 +0200

    Deprecate DatabaseChooser

--- bw2data/database.py
+++ bw2data/database.py
@@ -1,27 +1,7 @@
-from . import databases
-from .backends import SQLiteBackend
-from .backends.iotable import IOTableBackend
+from .backends import Database
+import warnings
 
 
 def DatabaseChooser(name, backend=None):
-    """A method that returns a database class instance.
-
-    Database types are specified in `databases[database_name]['backend']`.
-
-    """
-    if name in databases:
-        backend = databases[name].get("backend", backend or "sqlite")
-    else:
-        backend = backend or "sqlite"
-
-    # Backwards compatibility
-    if backend == "sqlite":
-        return SQLiteBackend(name)
-    elif backend == "iotable":
-        return IOTableBackend(name)
-    else:
-        raise ValueError("Backend {} not found".format(backend))
-
-
-# Backwards compatibility
-Database = DatabaseChooser
+    warnings.warn("`DatabaseChooser` is deprecated, use `Database` instead", DeprecationWarning)
+    return Database(name)
