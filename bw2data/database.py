d4743ba2ff20bb7ac8c538ffc49e769ec555af4e
Chris Mutel <cmutel@gmail.com>
2022-09-06 11:55:45 +0200

    Fix behaviour of DatabaseChooser when backend is not kwarg

--- bw2data/database.py
+++ bw2data/database.py
@@ -1,7 +1,18 @@
 from .backends import Database
+from .errors import UnknownObject
 import warnings
 
 
 def DatabaseChooser(name, backend=None):
     warnings.warn("`DatabaseChooser` is deprecated, use `Database` instead", DeprecationWarning)
-    return Database(name)
+    if Database.select().where(Database.name == name).count():
+        db = Database.get(Database.name == name)
+        if backend and db.backend != backend:
+            raise UnknownObject("Inconsistent backend: Database {} has backend {} but you gave {}".format(name, db.backend, backend))
+        return db
+    else:
+        db = Database()
+        db.name = name
+        if backend:
+            db.backend = backend
+        return db
