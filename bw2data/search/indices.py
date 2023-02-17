60997d34951a80f16b39fcceaf466a3ed59865c9
Chris Mutel <cmutel@gmail.com>
2023-02-17 21:33:51 +0100

    Merge pull request #115 from macmribo/json_importer
    Update indices.py

--- bw2data/search/indices.py
+++ bw2data/search/indices.py
@@ -25,7 +25,13 @@ class IndexManager:
         return index.create_in(self.path, bw2_schema)
 
     def _format_dataset(self, ds):
-        fl = lambda o: o[1].lower() if isinstance(o, tuple) else o.lower()
+        def fix_location(string):
+            string = string.strip()
+            if string.lower() == "none" or not string:
+                return ""
+            else:
+                return string
+
         return dict(
 
             name=(ds.get("name") or "").lower(),
@@ -33,7 +39,7 @@ class IndexManager:
             product=(ds.get("reference product") or "").lower(),
             categories=", ".join(ds.get("categories") or []).lower(),
             synonyms=", ".join(ds.get("synonyms") or []).lower(),
-            location=fl(ds.get("location") or ""),
+            location=fix_location(ds.get("location")),
             database=ds["database"],
             code=ds["code"],
         )
