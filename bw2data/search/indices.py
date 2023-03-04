1816a0a1754210a5049782bf0dc16755eccf0281
Benjamin W. Portner <benjaminwportner@gmail.com>
2023-03-04 19:29:16 +0100

    fix bw2data.search.indices._format_dataset: location must have a default value or tests are failing. Also, make sure tuple locations work as expected.

--- bw2data/search/indices.py
+++ bw2data/search/indices.py
@@ -25,12 +25,14 @@ class IndexManager:
         return index.create_in(self.path, bw2_schema)
 
     def _format_dataset(self, ds):
-        def fix_location(string):
-            string = string.strip()
-            if string.lower() == "none" or not string:
-                return ""
-            else:
-                return string
+        def _fix_location(string):
+            if isinstance(string, tuple):
+                string = string[1]
+            if isinstance(string, str):
+                if string.lower() == "none":
+                    return ""
+                else:
+                    return string.lower().strip()
 
         return dict(
             name=(ds.get("name") or "").lower(),
@@ -38,7 +40,7 @@ class IndexManager:
             product=(ds.get("reference product") or "").lower(),
             categories=", ".join(ds.get("categories") or []).lower(),
             synonyms=", ".join(ds.get("synonyms") or []).lower(),
-            location=fix_location(ds.get("location")),
+            location=_fix_location(ds.get("location") or ""),
             database=ds["database"],
             code=ds["code"],
         )
