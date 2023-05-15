57074158ba2c2da2c2109a4943d07bd225dbd966
Chris Mutel <cmutel@gmail.com>
2023-05-15 10:05:43 +0200

    Switch to new build infrastructure

---
+++ README.md
@@ -0,0 +1,7 @@
+Brightway2 Data Management
+==========================
+
+This package provides tools for the management of inventory databases and impact assessment methods.
+It is part of the [Brightway LCA framework](https://documnetation.brightway.dev). The source code is hosted on [GitHub brightway-lca organization](https://github.com/brightway-lca/brightway2-data).
+
+Note that version 4.0 and higher are only compatible with Brightway 2.5, as described in the [changelog](https://github.com/brightway-lca/brightway2-data/blob/main/CHANGES.md).
--- bw2data/__init__.py
+++ bw2data/__init__.py
@@ -30,7 +30,28 @@ __all__ = [
     "weightings",
 ]
 
-from .version import version as __version__
+import importlib
+from typing import Union
+
+
+def get_version_tuple() -> tuple:
+    """Returns version as (major, minor, micro)."""
+
+    def as_integer(version: str) -> Union[int, str]:
+        """Tries parsing version else returns as is."""
+        try:
+            return int(version)
+        except ValueError:  # pragma: no cover
+            return version  # pragma: no cover
+
+    return tuple(
+        as_integer(v)
+        for v in importlib.metadata.version("bw2data").strip().split(".")
+    )
+
+
+__version__ = get_version_tuple()
+
 
 from .configuration import config
 from .project import projects
