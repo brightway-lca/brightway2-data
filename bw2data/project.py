70befeb11c6b0776dd05fd5ad1fd688805907e3a
João Gonçalves <jsvgoncalves@gmail.com>
2024-11-04 10:12:10 +0100

    fixup! Allow first revision to be orphan

--- bw2data/project.py
+++ bw2data/project.py
@@ -45,6 +45,10 @@ def lockable():
     return False
 
 
+def new_snowflakeid() -> int:
+    return next(sfg(0))
+
+
 class ProjectDataset(Model):
     # Event sourcing
     is_sourced = BooleanField(default=False, constraints=[SQL("DEFAULT 0")])
